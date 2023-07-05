a15779a43b5089ab8621d5fc555194707c8b4fc0
adiyahoo <136817243+adiyahoo@users.noreply.github.com>
2023-07-05 11:02:30 +0700

    Update regex.rs

--- regex.rs
+++ regex.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 
 fn main() { 
    let re = Regex::new(r"(\w{5})").unwrap();
-   let text = "rhani"; 
+   let text = "adida"; 
 
    match re.captures(text) { 
     Some(caps) => println!("ada value: {}", caps.get(0).unwrap().as_str()),
